d4fa1163ff51b50f174cf70979dde3bb422a9614
Nazmul <nazmulalam1121@gmail.com>
2024-09-16 17:44:00 +0600

    Update index.js
    2nd question

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ function calculateDifference(a, b) {
   return a - b;
 }
 
-console.log(calculateDifference(10, 5));
+// console.log(calculateDifference(10, 5));
+
+// 2nd
+function isOdd(number) {
+  if (number % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+console.log(isOdd(5));
