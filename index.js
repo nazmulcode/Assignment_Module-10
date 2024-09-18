cf6a2bfe7f0394788a828417709161dadf2237ff
Nazmul <nazmulalam1121@gmail.com>
2024-09-18 09:33:35 +0600

    All

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function calculateDifference(a, b) {
   return a - b;
 }
 
-// console.log(calculateDifference(10, 5));
+console.log(calculateDifference(10, 5));
 
 // 2nd
 function isOdd(number) {
@@ -14,14 +14,14 @@ function isOdd(number) {
   }
 }
 
-// console.log(isOdd(5));
+console.log(isOdd(5));
 
 // 3rd
 let numbers3 = [2, 4, 5, 7, 1, 8];
 function findMin(num) {
   return Math.min(...num);
 }
-// console.log(findMin(numbers3));
+console.log(findMin(numbers3));
 
 // 4th
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -29,7 +29,7 @@ function filterEvenNumbers(number) {
   return number.filter((num) => num % 2 === 0);
 }
 let evenNumbers = filterEvenNumbers(numbers);
-// console.log(evenNumbers);
+console.log(evenNumbers);
 
 // 5th
 let number = [20, 10, 15, 70, 50, 30];
@@ -43,14 +43,14 @@ console.log(newArr);
 function lowercaseFirstLetter(name) {
   return name.charAt(0).toLowerCase() + name.slice(1);
 }
-// console.log(lowercaseFirstLetter("NAZMUL"));
+console.log(lowercaseFirstLetter("NAZMUL"));
 
 // 7th
 function countVowels(name) {
   const vowels = "aeiouAEIOU";
   return Array.from(name).filter((countV) => vowels.includes(countV)).length;
 }
-// console.log(countVowels("Nazmul"));
+console.log(countVowels("Nazmul"));
 
 // 8th
 let numbers2 = [10, 20, 30, 40, 50];
@@ -58,4 +58,4 @@ function findAverage(num) {
   let total = num.reduce((acc, val) => acc + val, 0);
   return total / num.length;
 }
-// console.log(findAverage(numbers2));
+console.log(findAverage(numbers2));
